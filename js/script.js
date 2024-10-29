fe57259e1f9651b97100e5968e01a78a64a214d3
Maurício Mendes <mauriciodarth@gmail.com>
2024-10-29 09:29:58 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var titulo = (document.querySelector("h1"));
+console.log(titulo.textContent = "Aparecida nutricionista");
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura")
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector(".info-imc");
+var imc = peso / (altura * altura);
+tdImc.textContent = imc;
